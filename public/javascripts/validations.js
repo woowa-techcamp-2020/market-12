dc41f26f5cfc6e2a1f79858ca64951158bb4435b
Atobaum <atobaum@gmail.com>
2020-07-07 02:27:19 +0900

    [추가] 회원가입 페이지 인풋 검사 파일 추기

---
+++ public/javascripts/validations.js
@@ -0,0 +1,26 @@
+/**
+ * @param  {string} id
+ * @returns {boolean}
+ */
+function validateId(id) {}
+/**
+ * @param  {string} password
+ * @returns {boolean}
+ */
+function validatePassword(password) {}
+/**
+ * @param  {string} username
+ * @param  {string} provider
+ * @returns {boolean}
+ */
+function validateEmail(username, provider) {}
+/**
+ * @param  {string} name
+ * @returns {boolean}
+ */
+function validateName(name) {}
+/**
+ * @param  {string} phoneNumber
+ * @returns {boolean}
+ */
+function validatePhoneNumber(phoneNumber) {}
