09189c9098aa82e9387281f93faddaa518068b73
이찬호 <0407chan@naver.com>
2020-07-06 22:59:06 +0900

    [추가] register api 생성 #30
    프론트와 연결 확인

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.post("/register", function (req, res) {
+  let user = req.body;
+
+  console.log(user);
+
+  res.send("로그인 완료");
+});
+
 module.exports = router;
