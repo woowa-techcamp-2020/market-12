7410ea24c32696267049c4a212efe054009d49fd
Atobaum <atobaum@gmail.com>
2020-07-06 16:01:35 +0900

    [추가] 라우트 추가

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "배민상회22" });
+  res.render("index", { title: "Express" });
+});
+
+/* GET login page. */
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+/* GET register page. */
+router.get("/register", function (req, res, next) {
+  res.render("register");
+});
+
+/* GET compelete register page. */
+router.get("/complete_register", function (req, res, next) {
+  res.render("complete_register", {
+    name: "name",
+    id: "id",
+    email: "email",
+    phone: "010",
+  });
 });
 
 module.exports = router;
