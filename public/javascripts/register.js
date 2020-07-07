1e39abc1bb52d6351926b4b45547ce58e45f99eb
Atobaum <atobaum@gmail.com>
2020-07-07 09:31:44 +0900

    [수정] 회원가입 페이지 id validation 추가

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -10,3 +10,9 @@ function applyValidation(inputId, validation, label) {
     labelDom.innerText = label(validation(e.target.value));
   });
 }
+
+applyValidation("input_id", validateId, (valid) =>
+  valid
+    ? "입력하신 아이디로 사용이 가능합니다."
+    : "아이디는 4~20자의 영 소문자, 숫자, 특수기호(_), (-)만 사용 가능합니다. "
+);
