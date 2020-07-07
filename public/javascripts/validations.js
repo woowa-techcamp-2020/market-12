564f1d681dcb30589a60dc0fa2802418f75b1a65
Atobaum <atobaum@gmail.com>
2020-07-07 10:26:44 +0900

    [수정] 이메일 체크 함수 분리
    username과 provider를 체크하는 부분을 분리했습니다.

--- public/javascripts/validations.js
+++ public/javascripts/validations.js
@@ -15,20 +15,26 @@ function validatePassword(password) {
 }
 /**
  * @param  {string} username
+ * @returns {boolean}
+ */
+function validateEmailUsername(username) {
+  //https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression 참고함
+  return;
+  /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")$/.test(
+    username
+  );
+}
+
+/**
  * @param  {string} provider
  * @returns {boolean}
  */
-function validateEmail(username, provider) {
-  //https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression 참고함
-  return (
-    /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")$/.test(
-      username
-    ) &&
-    /^(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(
-      provider
-    )
+function validateEmailPrivider(provider) {
+  return /^(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(
+    provider
   );
 }
+
 /**
  * @param  {string} name
  * @returns {boolean}
