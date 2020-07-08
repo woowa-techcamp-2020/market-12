ee9df21449ecc29da2fffaa3ab8c0903d8d149fa
Atobaum <atobaum@gmail.com>
2020-07-08 14:34:30 +0900

    [수정] 로그인 시 메인 페이지에서 유저정보 표시

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "배민상회", user: req.session.user });
 });
 
 /* GET login page. */
