86182e5e1b182559da193e7f6bd1f8a7b291f8b4
Atobaum <atobaum@gmail.com>
2020-07-08 10:13:57 +0900

    [추가] Express에 session 연결 (#25)
    사용법은 위키에 참조

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+const session = require("./lib/sessionMiddleware");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -17,6 +18,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
